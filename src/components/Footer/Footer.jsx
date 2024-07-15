7f121465c2a320ddbf5106588c5efdd324e4804e
Ken-2100 <166726504+Ken-2100@users.noreply.github.com>
2024-07-15 16:11:33 +0800

    fix foote

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,10 +3,10 @@ import logoIcon from './logo-transparent.png';
 
 export default function Footer() {
     return(
-        <footer>
-        <section id='custom-bg-color' >
-        <div className="container">
-  <div className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+
+        <footer id='custom-bg-color' className='container-fluid' >
+       
+  <div className="d-flex flex-wrap justify-content-between align-items-center py-3  border-top container">
     <p className="col-md-4 mb-0 text-body-secondary">© 2024 Kodego Students, Ken & John</p>
 
     <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
@@ -19,8 +19,8 @@ export default function Footer() {
       <li className="nav-item"><a href="#AboutUs" className="nav-link px-2 text-body-secondary">About</a></li>
     </ul>
   </div>
-</div>
-        </section>
+
         </footer>
+
     );
 }
